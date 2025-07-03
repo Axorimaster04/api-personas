654dcc70db86e6675abe087ac541cc14e59beaf3
Axorimaster04 <rodrigo.zambrano.b@utec.edu.pe>
2025-07-03 09:02:56 -0500

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,7 +1,16 @@
 from pydantic import BaseModel
+
 class Persona(BaseModel):
     codigo_persona: int
     tipo_persona: str
     indicador_lista_negra: int
     nacionalidad: str
     pais_procedencia: str
+    
+class Poliza(BaseModel):
+    codigo_persona: int
+    codigo_poliza: int
+    numero_poliza: int
+    nombre_producto: str
+    desc_producto: str
+    cat_producto: str
